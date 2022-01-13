3a3fda10add6b86b4bc6e7f79f1533ae3d2dde6a
zx <zhangx@33.cn>
2022-01-13 16:07:06 +0800

    init

--- app/src/main/java/com/w/walletapi/MainActivity.kt
+++ app/src/main/java/com/w/walletapi/MainActivity.kt
@@ -2,6 +2,7 @@ package com.w.walletapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import walletapi.Walletapi
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
